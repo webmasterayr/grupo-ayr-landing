fa86050239ebe54901040ddde1ef65ecc99ba236
keven-bardales <keven.bardales@gmail.com>
2025-05-09 10:35:04 -0600

    fix: linkning

--- src/components/ui/AnchorLink.tsx
+++ src/components/ui/AnchorLink.tsx
@@ -1,7 +1,7 @@
-"use client";
-import React from "react";
-import handleLandingElementIntoView from "@/modules/shared/presentation/utils/handle-into-view/handle-element-into-view";
-import Link from "next/link";
+'use client';
+import React from 'react';
+import handleLandingElementIntoView from '@/modules/shared/presentation/utils/handle-into-view/handle-element-into-view';
+import Link from 'next/link';
 
 interface AnchorLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   href: string;
@@ -11,36 +11,27 @@ interface AnchorLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> 
 }
 
 // Supports both #section and normal links
-const AnchorLink = React.forwardRef<HTMLAnchorElement, AnchorLinkProps>(
-  ({ href, offset = 0, children, className = "", ...rest }, ref) => {
-    const isAnchor = href.startsWith("#") || (href.startsWith("/") && href.includes("#"));
+const AnchorLink = React.forwardRef<HTMLAnchorElement, AnchorLinkProps>(({ href, offset = 0, children, className = '', ...rest }, ref) => {
+  const isAnchor = href.startsWith('#') || (href.startsWith('/') && href.includes('#'));
 
-    const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-      if (isAnchor) {
-        const hashIndex = href.indexOf("#");
-        if (hashIndex !== -1) {
-          e.preventDefault();
-          const id = href.substring(hashIndex + 1);     
-          handleLandingElementIntoView(id, offset);
-          // Update hash in URL without scrolling
-        }
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (isAnchor) {
+      const hashIndex = href.indexOf('#');
+      if (hashIndex !== -1) {
+        const id = href.substring(hashIndex + 1);
+        handleLandingElementIntoView(id, offset);
+        // Update hash in URL without scrolling
       }
-      if (rest.onClick) rest.onClick(e);
-    };
+    }
+    if (rest.onClick) rest.onClick(e);
+  };
 
-    return (
-      <Link   
-        href={href}
-        ref={ref}
-        className={className}
-        onClick={handleClick}
-        {...rest}
-      >
-        {children}
-      </Link>
-    );
-  }
-);
-AnchorLink.displayName = "AnchorLink";
+  return (
+    <Link href={href} ref={ref} className={className} onClick={handleClick} {...rest}>
+      {children}
+    </Link>
+  );
+});
+AnchorLink.displayName = 'AnchorLink';
 
 export default AnchorLink;
