1b8b19e52fdd17fb93235981f8694a49a112998e
keven-bardales <keven.bardales@gmail.com>
2025-05-09 10:37:59 -0600

    fix: scroll

--- src/components/ui/AnchorLink.tsx
+++ src/components/ui/AnchorLink.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import handleLandingElementIntoView from '@/modules/shared/presentation/utils/handle-into-view/handle-element-into-view';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface AnchorLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   href: string;
@@ -14,16 +15,17 @@ interface AnchorLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> 
 const AnchorLink = React.forwardRef<HTMLAnchorElement, AnchorLinkProps>(({ href, offset = 0, children, className = '', ...rest }, ref) => {
   const isAnchor = href.startsWith('#') || (href.startsWith('/') && href.includes('#'));
 
+  const pathName = usePathname();
+
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    if (isAnchor) {
+    if (isAnchor && pathName == '/') {
       const hashIndex = href.indexOf('#');
       if (hashIndex !== -1) {
+        e.preventDefault();
         const id = href.substring(hashIndex + 1);
         handleLandingElementIntoView(id, offset);
-        // Update hash in URL without scrolling
       }
     }
-    if (rest.onClick) rest.onClick(e);
   };
 
   return (
